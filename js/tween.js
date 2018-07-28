fb52ff0ca2422353f4da1f341535dbe5b9195583
sexfat <bryant6831@gmail.com>
2018-07-28 15:46:48 +0800

    add section01

--- js/tween.js
+++ js/tween.js
@@ -1,1 +1,22 @@
-console.log("ok");
+console.log("index ok");
+
+TweenMax.fromTo(['.box_01' ,'.box_02'], 3, {
+    x: 0
+    // y: 0
+}, {
+    x: 200,
+    // ease: Bounce.easeOut
+    // scale: 10
+    // y: 100,
+});
+
+
+TweenMax.fromTo('.box_02', 3, {
+    // x: 0
+    y: 0
+}, {
+    y: 200,
+    // ease: Bounce.easeOut
+    // scale: 10
+    // y: 100,
+});
