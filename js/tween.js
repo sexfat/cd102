a9bbd6fccb51ec08101742dbb7734278b45f77f4
sexfat <bryant6831@gmail.com>
2018-07-28 16:09:13 +0800

    add from

--- js/tween.js
+++ js/tween.js
@@ -15,8 +15,26 @@ TweenMax.fromTo('.box_02', 3, {
     // x: 0
     y: 0
 }, {
-    y: 200,
+    y: 10,
     // ease: Bounce.easeOut
     // scale: 10
     // y: 100,
 });
+
+
+TweenMax.fromTo('.box_03', 1, {
+    x: 0
+}, {
+    x: 100 
+});
+
+TweenMax.fromTo('.box_04', 1, {
+    x: 0
+}, {
+    x: 100,
+    delay: 1,
+    repeat : -1,
+    yoyo: true
+    // repeatDelay : 1
+});
+
