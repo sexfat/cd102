83fa8ad7d784ab8f5583eda2f89458e4e26bc952
sexfat <bryant6831@gmail.com>
2018-07-28 16:27:30 +0800

    add bezier

--- js/tween.js
+++ js/tween.js
@@ -33,8 +33,32 @@ TweenMax.fromTo('.box_04', 1, {
 }, {
     x: 100,
     delay: 1,
-    repeat : -1,
-    yoyo: true
+    repeat : 1,
+    yoyo: true,
+    rotationZ: 180,  //旋轉
+    transformOrigin : 'center top' //定位點
     // repeatDelay : 1
 });
 
+
+TweenMax.fromTo('.box_05', 4, {
+    x: 0
+}, {
+    bezier: {
+        // curviness: 1.25,
+        values: [{
+            x: 100,
+            y: 200
+        }, {
+            x: 250,
+            y: 400
+        },{
+            x: 500,
+            y: 800
+        },{
+            x: 0,
+            y: 0
+        }],
+        // autoRotate: true
+    },
+});
